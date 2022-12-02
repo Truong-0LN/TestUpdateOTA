22a0b953f57fbf7ab20aa02b3cbdd36c8cee1dbf
Truong-0LN <93598071+Truong-0LN@users.noreply.github.com>
2022-12-02 10:16:34 +0700

    fix update ota

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-
+import codePush from 'react-native-code-push';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>App</Text>
+      <Text style={styles.text}>App</Text>
     </View>
   );
 };
 
-export default App;
+export default codePush(App);
 
 const styles = StyleSheet.create({
+  text: {
+    fontSize: 12,
+    fontWeight: '700',
+    color: '#02a',
+  },
   container: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#acd',
+    backgroundColor: '#fff',
     flex: 1,
   },
 });
